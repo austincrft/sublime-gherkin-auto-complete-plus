4f35fcf722c29ea0474d24f850570a83983f52f1
Craft, Austin <austin.craft@tylertech.com>
2015-10-12 15:55:46 -0500

    added gherkin language file, removed intermediate text file, fixed some small issues

--- gherkin_event_listener.py
+++ gherkin_event_listener.py
@@ -1,14 +1,82 @@
 import sublime
 import sublime_plugin
 import re
+import GherkinAutocompletePlus.update_steps as update_steps
 
 settings = sublime.load_settings('gherkin_autocomplete_plus.sublime-settings')
 filepath = settings.get('step_path')
 keywords = ['given', 'when', 'then']
 completions = {}
+steps = []
 
 
-def is_gherkin_scope(view):
+class GherkinEventListener(sublime_plugin.EventListener):
+    """
+    Sublime Text Gherkin AutoComplete integration
+    """
+
+    def __init__(self):
+        self.first_modify = True
+
+    def on_modified(self, view):
+        """ Triggers when a sublime.View is modified. If in Gherkin syntax,
+            opens AutoComplete menu and fills with completions.
+
+        :param view: sublime.View object
+        :type view: sublime.View
+        """
+        view_sel = view.sel()
+        if not view_sel:
+            return
+
+        if not self._is_gherkin_scope(view):
+            return
+
+        if self.first_modify:
+            self._update_steps()
+            self.first_modify = False
+
+        view.settings().set('auto_complete', False)
+
+        pos = view_sel[0].end()
+        next_char = view.substr(sublime.Region(pos - 1, pos))
+
+        if next_char in (' ', '\n'):
+            view.run_command('hide_auto_complete')
+            return
+
+        view.run_command('hide_auto_complete')
+        self._show_auto_complete(view)
+        self._fill_completions(view, pos)
+
+    def on_query_completions(self, view, prefix, locations):
+        """ Sublime Text AutoComplete event handler
+
+        Takes the completions that were set in the 'fill_completions' method
+        and returns them to the AutoComplete list.
+
+        :param view: `sublime.View` object
+        :type view: sublime.View
+        :param prefix: last word to the left of the cursor
+        :type prefix: str
+        :param locations: offset from beginning of line
+        :type locations: list(int)
+
+        ^^ None of which are used, but have some documentation anyway.
+        """
+        _completions = [sug for key, sug in completions.items()]
+        completions.clear()
+
+        return sorted(_completions)
+
+    def on_post_save_async(self, view):
+        self._update_steps()
+
+    def _update_steps(self):
+        steps.clear()
+        [steps.append(step) for step in update_steps.run()]
+
+    def _is_gherkin_scope(self, view):
         """ Validates that user is in Gherkin Syntax
 
         :param view: `sublime.View` object
@@ -19,8 +87,7 @@ def is_gherkin_scope(view):
         location = view.sel()[0].end()
         return view.match_selector(location, "text.gherkin.feature - comment")
 
-
-def step_matches_line(step_words, line_words):
+    def _step_matches_line(self, step_words, line_words):
         """ Validates that words in step match words in line
 
         :param step_words: words in step definition
@@ -43,168 +110,105 @@ def step_matches_line(step_words, line_words):
         else:
             return False
 
+    def _format_step(self, step, line_words=[]):
+        """ Returns step formatted in snippet notation
 
-def format_step(step, line_words=[]):
-    """ Returns step formatted in snippet notation
+        :param step: step definition
+        :type step: str
+        :param line_words: words in step definition
+        :type line_words: list(str)
 
-    :param step: step definition
-    :type step: str
-    :param line_words: words in step definition
-    :type line_words: list(str)
+        :return: str
+        """
+        # Skip first word in line because it is a keyword
+        # Skip last word in line so it'll be included in output
+        line_text = ' '.join(line_words[1:-1])
 
-    :return: str
-    """
-    # Skip first word in line because it is a keyword
-    # Skip last word in line so it'll be included in output
-    line_text = ' '.join(line_words[1:-1])
+        for i in range(len(line_text)):
+            step = step.replace(line_text[i], '', 1)
+        index = 1
+        regex = r'((?:\".+?\")|(?:\'.+?\')|(?:\<.+?\>)|(?:\[number\]))'
+        replace_values = re.findall(regex, step)
+        for word in replace_values:
+            if word[0] == '"':
+                step = step.replace(word, '"${' + str(index) + ':input}"', 1)
+                index += 1
+            elif word[0] == "'":
+                step = step.replace(word, "'${" + str(index) + ":input}'", 1)
+                index += 1
+            elif word[0] == '<':
+                step = step.replace(word, '<${' + str(index) + ':input}>', 1)
+                index += 1
+            elif word[0] == '[':
+                step = step.replace(word, '${' + str(index) + ':[number]}', 1)
+                index += 1
 
-    for i in range(len(line_text)):
-        step = step.replace(line_text[i], '', 1)
-    index = 1
-    regex = r'((?:\".+?\")|(?:\'.+?\')|(?:\<.+?\>)|(?:\[number\]))'
-    replace_values = re.findall(regex, step)
-    for word in replace_values:
-        if word[0] == '"':
-            step = step.replace(word, '"${' + str(index) + ':input}"', 1)
-            index += 1
-        elif word[0] == "'":
-            step = step.replace(word, "'${" + str(index) + ":input}'", 1)
-            index += 1
-        elif word[0] == '<':
-            step = step.replace(word, '<${' + str(index) + ':input}>', 1)
-            index += 1
-        elif word[0] == '[':
-            step = step.replace(word, '${' + str(index) + ':[number]}', 1)
-            index += 1
+        return step.strip()
 
-    return step.strip()
+    def _show_auto_complete(self, view):
+        """ Opens AutoComplete manually
 
-
-def show_auto_complete(view):
-    """ Opens AutoComplete manually
-
-    :param view: the view containing the cursor
-    :type view: sublime.View
-    """
-    def _show_auto_complete():
-        view.run_command('auto_complete', {
-            'disable_auto_insert': True,
-            'api_completions_only': True,
-            'next_completion_if_showing': False,
-            'auto_complete_commit_on_tab': True,
-        })
-    # Have to set a timeout for some reason
-    sublime.set_timeout(_show_auto_complete, 0)
-
-
-def fill_completions(view, location):
-    """ Gets completions from file specified in 'step_path' and adds them to
-        'completions' list
-
-    :param view: `sublime.View` object
-    :type view: sublime.View
-    :param location: position of cursor in line
-    :type locations: int
-    """
-    last_keyword = ''
-    current_region = view.line(location)
-    current_line_text = view.substr(current_region).strip()
-    current_line_words = current_line_text.split()
-
-    # If first word is keyword, take that one
-    if current_line_words and current_line_words[0].lower() in keywords:
-        last_keyword = current_line_words[0].lower()
-    # Otherwise, reverse iterate through lines until keyword is found
-    else:
-        all_lines = view.split_by_newlines(sublime.Region(0, view.size()))
-        current_index = all_lines.index(current_region)
-        for region in reversed(all_lines[0:current_index]):
-            region_text = view.substr(region).lstrip()
-            split_line = region_text.split(' ', 1)
-            if split_line and split_line[0].lower() in keywords:
-                last_keyword = split_line[0].lower()
-                break
-
-    if not last_keyword:
-        print("GherkinAutocompletePlus: Could not find 'Given', 'When', "
-              "or 'Then' in text.")
-        return
-
-    suggestions = []
-    try:
-        with open(filepath) as step_file:
-            for line in step_file.readlines():
-                split_lines = line.split(',', 1)
-                if split_lines and last_keyword == split_lines[0]:
-                    step_type = split_lines[0]
-                    step = split_lines[1]
-
-                    # If only keyword is typed, provide all steps for keyword
-                    if len(current_line_words) == 1:
-                        step_format = format_step(step)
-                        suggestion = (step + '\t' + step_type, step_format)
-                        completions[step] = suggestion
-
-                    # If more words typed, check for match
-                    elif len(current_line_words) > 1:
-                        if step_matches_line(step.split(), current_line_words):
-                            step_format = format_step(step, current_line_words)
-                            suggestion = (step + '\t' + step_type, step_format)
-                            completions[step] = suggestion
-    except FileNotFoundError as e:
-        sublime.error_message('GherkinAutocompletePlus:\n\n' + str(e))
-
-
-class GherkinEventListener(sublime_plugin.EventListener):
-    """
-    Sublime Text Gherkin AutoComplete integration
-    """
-
-    def on_modified(self, view):
-        """ Triggers when a sublime.View is modified. If in Gherkin syntax,
-            opens AutoComplete menu and fills with completions.
-
-        :param view: sublime.View object
+        :param view: the view containing the cursor
         :type view: sublime.View
         """
-        view_sel = view.sel()
-        if not view_sel:
-            return
+        def _show_auto_complete():
+            view.run_command('auto_complete', {
+                'disable_auto_insert': True,
+                'api_completions_only': True,
+                'next_completion_if_showing': False,
+                'auto_complete_commit_on_tab': True,
+            })
+        # Have to set a timeout for some reason
+        sublime.set_timeout(_show_auto_complete, 0)
 
-        if not is_gherkin_scope(view):
-            return
-
-        view.settings().set('auto_complete', False)
-
-        pos = view_sel[0].end()
-        next_char = view.substr(sublime.Region(pos - 1, pos))
-
-        if next_char in (' ', '\n'):
-            view.run_command('hide_auto_complete')
-            return
-
-        view.run_command('hide_auto_complete')
-        show_auto_complete(view)
-        fill_completions(view, pos)
-
-    def on_query_completions(self, view, prefix, locations):
-        """ Sublime Text AutoComplete event handler
-
-        Takes the completions that were set in the 'fill_completions' method
-        and returns them to the AutoComplete list.
+    def _fill_completions(self, view, location):
+        """ Gets completions from file specified in 'step_path' and adds them to
+            'completions' list
 
         :param view: `sublime.View` object
         :type view: sublime.View
-        :param prefix: last word to the left of the cursor
-        :type prefix: str
-        :param locations: offset from beginning of line
-        :type locations: list(int)
-
-        ^^ None of which are used, but have some documentation anyway.
+        :param location: position of cursor in line
+        :type locations: int
         """
-        _completions = []
+        last_keyword = ''
+        current_region = view.line(location)
+        current_line_text = view.substr(current_region).strip()
+        current_line_words = current_line_text.split()
 
-        [_completions.append(sug) for key, sug in completions.items()]
-        completions.clear()
+        # Don't fill completions until after first space is typed
+        if ' ' not in current_line_text:
+            return
 
-        return sorted(_completions)
+        # If first word is keyword, take that one
+        if current_line_words and current_line_words[0].lower() in keywords:
+            last_keyword = current_line_words[0].lower()
+        # Otherwise, reverse iterate through lines until keyword is found
+        else:
+            all_lines = view.split_by_newlines(sublime.Region(0, view.size()))
+            current_index = all_lines.index(current_region)
+            for region in reversed(all_lines[0:current_index]):
+                region_text = view.substr(region).lstrip()
+                split_line = region_text.split(' ', 1)
+                if split_line and split_line[0].lower() in keywords:
+                    last_keyword = split_line[0].lower()
+                    break
+
+        if not last_keyword:
+            print("GherkinAutocompletePlus: Could not find 'Given', 'When', "
+                  "or 'Then' in text.")
+            return
+
+        for step_type, step in steps:
+            if step_type == last_keyword:
+                # If only keyword is typed, provide all steps for keyword
+                if len(current_line_words) == 1:
+                    step_format = self._format_step(step)
+                    suggestion = (step + '\t' + step_type, step_format)
+                    completions[step] = suggestion
+
+                # If more words typed, check for match
+                elif len(current_line_words) > 1:
+                    if self._step_matches_line(step.split(), current_line_words):
+                        step_format = self._format_step(step, current_line_words)
+                        suggestion = (step + '\t' + step_type, step_format)
+                        completions[step] = suggestion
--- update_steps.py
+++ update_steps.py
@@ -1,4 +1,4 @@
-import os
+import glob
 import re
 import sublime
 import sublime_plugin
@@ -6,12 +6,7 @@ import sublime_plugin
 feature_error = ('GherkinAutocompletePlus: \nPlease specify a path to the directory'
                  ' containing your Feature Files. Go to \n\n"Preferences -> '
                  'Package Settings -> GherkinAutocompletePlus -> Settings - User"'
-                 '\n\n and create an entry for "feature_file_directory".')
-
-definition_error = ('GherkinAutocompletePlus: \nPlease specify a path to the file '
-                    'where you would like to keep the step definitions. Go to \n\n'
-                    '"Preferences -> Package Settings -> GherkinAutocompletePlus -> '
-                    'Settings - User"\n\n and create an entry for "step_path".')
+                 '\n\n and create an entry for "feature_file_directories".')
 
 
 def is_int(s):
@@ -22,74 +17,64 @@ def is_int(s):
         return False
 
 
-class UpdateStepsCommand(sublime_plugin.TextCommand):
-    """
-    Sublime Text command for updating step definitions
-    """
+def format_step(line):
+    # SWEET MOTHER OF REGEX!
+    # Get values in between single- and double-quotes,
+    # values in between greater- and less-than signs,
+    # and numbers in 'integer' and 'decimal' format
+    replace_values = re.findall(
+        r'((?:\".+?\")|(?:\'.+?\')|(?:\<.+?\>)|(?:\d+(?:\.\d*)?|(?:\.\d+)))',
+        line)
 
-    def run(self, edit):
-        settings = sublime.load_settings('gherkin_autocomplete_plus.sublime-settings')
-        feature_file_directory = settings.get('feature_file_directory')
+    for word in replace_values:
+        if word:
+            if word[0] == '"':
+                line = line.replace(word, '"input"', 1)
+            elif word[0] == "'":
+                line = line.replace(word, "'input'", 1)
+            elif word[0] == '<':
+                line = line.replace(word, '<input>', 1)
+            elif is_int(word[0]) or word[0] == '.':
+                line = line.replace(word, "[number]", 1)
 
-        if feature_file_directory == 'path/to/featurefiles/directory':
-            sublime.error_message(feature_error)
-            return None
+    return line
 
-        if not feature_file_directory.endswith('/'):
-            feature_file_directory += '/'
 
-        step_file_path = settings.get('step_path')
+def run():
+    settings = sublime.load_settings('gherkin_autocomplete_plus.sublime-settings')
+    feature_file_directories = settings.get('feature_file_directories')
 
-        if step_file_path == 'path/to/steps.txt':
-            sublime.error_message()
+    if not feature_file_directories:
+        sublime.error_message(feature_error)
+        return None
 
-        main_words = ['given', 'when', 'then']
-        extra_words = ['and', 'but']
+    main_words = ['given', 'when', 'then']
+    extra_words = ['and', 'but']
 
-        lines = set()
-        last_main_word = ''
+    lines = set()
+    last_main_word = ''
 
-        for file_ in os.listdir(feature_file_directory):
-            if file_.endswith('.feature'):
-                with open(feature_file_directory + file_) as feature:
-                    for line in feature.readlines():
-                        # Only separate first word in line
-                        line_split = line.split(maxsplit=1)
-                        first_word = line_split[0].lower() if line_split else ''
+    for path in feature_file_directories:
+        if not path.endswith('/'):
+            path += '/'
+        for file_ in glob.glob(path + '*.feature'):
+            with open(file_) as feature:
+                for line in feature.readlines():
+                    # Only separate first word in line
+                    line_split = line.split(maxsplit=1)
+                    first_word = line_split[0].lower() if line_split else ''
 
-                        if not first_word:
-                            continue
-                        elif first_word in main_words:
-                            last_main_word = first_word.lower()
-                        elif first_word in extra_words:
-                            pass
-                        else:
-                            continue
+                    if not first_word:
+                        continue
+                    elif first_word in main_words:
+                        last_main_word = first_word.lower()
+                    elif first_word in extra_words:
+                        pass
+                    else:
+                        continue
 
-                        # Remove keyword from line
-                        line = line_split[1]
+                    # Remove keyword from line and format step
+                    line = format_step(line_split[1])
+                    lines.add((last_main_word, line.strip() + '\n'))
 
-                        # SWEET MOTHER OF REGEX!
-                        # Get values in between single- and double-quotes,
-                        # values in between greater- and less-than signs,
-                        # and numbers in 'integer' and 'decimal' format
-                        replace_values = re.findall(
-                            r'((?:\".+?\")|(?:\'.+?\')|(?:\<.+?\>)|(?:\d+(?:\.\d*)?|(?:\.\d+)))',
-                            line)
-
-                        for word in replace_values:
-                            if word:
-                                if word[0] == '"':
-                                    line = line.replace(word, '"input"', 1)
-                                elif word[0] == "'":
-                                    line = line.replace(word, "'input'", 1)
-                                elif word[0] == '<':
-                                    line = line.replace(word, '<input>', 1)
-                                elif is_int(word[0]) or word[0] == '.':
-                                    line = line.replace(word, "[number]", 1)
-
-                        lines.add((last_main_word, line.strip() + '\n'))
-
-        with open(step_file_path, 'w') as csv:
-            csv.write("Type,Body\n")
-            [csv.write(line[0] + ',' + line[1]) for line in lines]
+    return lines
