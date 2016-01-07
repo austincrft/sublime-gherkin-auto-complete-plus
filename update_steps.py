10bc47972bfdc5a63d89f4a4dec54cfe6a2ee47a
unknown <austin.craft@PLADPDTACRAFT.corp.tylertechnologies.com>
2016-01-07 11:00:56 -0600

    bug fixes, light refactoring

--- update_steps.py
+++ update_steps.py
@@ -3,13 +3,13 @@ import re
 import sublime
 import sublime_plugin
 
-feature_error = ('GherkinAutocompletePlus: \nPlease specify a path to the directory'
+feature_error = ('Gherkin Auto-Complete Plus: \nPlease specify a path to the directory'
                  ' containing your Feature Files. Go to \n\n"Preferences -> '
-                 'Package Settings -> GherkinAutocompletePlus -> Settings - User"'
+                 'Package Settings -> Gherkin Auto-Complete Plus -> Settings - User"'
                  '\n\n and create an entry for "feature_file_directories".')
 
 
-def is_int(s):
+def _is_int(s):
     """ Evaluates whether provided string is an integer
 
     :param s: input string
@@ -22,13 +22,12 @@ def is_int(s):
         return False
 
 
-def format_step(step):
+def _format_step(step):
     """ Formats Gherkin step in snippet notation
 
-    :param step: Gherkin step
-    :type step: str
-
-    :return: str
+    :param str step: Gherkin step
+    :return: formateted Gherkin step
+    :rtype: str
     """
     # SWEET MOTHER OF REGEX!
     # Get values in between single- and double-quotes,
@@ -46,17 +45,43 @@ def format_step(step):
                 step = step.replace(word, "'input'", 1)
             elif word[0] == '<':
                 step = step.replace(word, '<input>', 1)
-            elif is_int(word[0]) or word[0] == '.':
+            elif _is_int(word[0]) or word[0] == '.':
                 step = step.replace(word, "[number]", 1)
 
     return step
+
+def _get_steps_from_files(files=None):
+    if not files:
+        files = []
+
+    with open(file_) as feature:
+        for line in feature.readlines():
+            # Separate keyword from line
+            line_split = line.split(maxsplit=1)
+
+            # Skip line if no step body is present
+            if len(line_split) < 2:
+                continue
+
+            first_word = line_split[0].lower()
+
+            elif first_word in main_words:
+                last_main_word = first_word.lower()
+            elif first_word in extra_words:
+                pass
+            else:
+                continue
+
+            # Remove keyword from line and format step
+            line = _format_step(line_split[1])
+            lines.add((last_main_word, line.strip() + '\n'))
 
 
 def run():
     """ Main method of the module, iterates through directories provided
         in the settings to store and format the Gherkin steps.
 
-    :return: list(tuple(str, str))
+    :rtype: list(tuple(str, str))
     """
     settings = sublime.load_settings('Gherkin Auto-Complete Plus.sublime-settings')
     feature_file_directories = settings.get('feature_file_directories')
@@ -75,23 +100,5 @@ def run():
         if not path.endswith('/'):
             path += '/'
         for file_ in glob.glob(path + '*.feature'):
-            with open(file_) as feature:
-                for line in feature.readlines():
-                    # Only separate first word in line
-                    line_split = line.split(maxsplit=1)
-                    first_word = line_split[0].lower() if line_split else ''
-
-                    if not first_word:
-                        continue
-                    elif first_word in main_words:
-                        last_main_word = first_word.lower()
-                    elif first_word in extra_words:
-                        pass
-                    else:
-                        continue
-
-                    # Remove keyword from line and format step
-                    line = format_step(line_split[1])
-                    lines.add((last_main_word, line.strip() + '\n'))
 
     return lines
